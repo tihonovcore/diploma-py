fc0a80481262540e94ad386de952613c02a61f9e
tihonovcore <tihonovcore@gmail.com>
2021-05-07 13:28:53 +0300

    [types] add new subtype questions

--- type_embeddings/question.py
+++ type_embeddings/question.py
@@ -16,7 +16,7 @@ class Question(keras.Model):
     ):
         super(Question, self).__init__(name='question_model', **kwargs)
 
-        self.question_count = 2
+        self.question_count = 5
 
         self.type_embeddings = TE()
 
@@ -24,62 +24,102 @@ class Question(keras.Model):
 
         self.subtype = KInputsNN(k=2)
         self.has_as_members = KInputsNN(k=2)
-        self.callable_with = KInputsNN(k=2)
-        self.has_members = KInputsNN(k=3)
+        self.returns = KInputsNN(k=3)
+
+        self.cnt = [0 for _ in range(self.question_count)]
 
     def call(self, inputs, **kwargs):
         class_embeddings, function_embeddings, member_function_embeddings = self.type_embeddings.call(inputs)
 
         class_id_count = len(inputs["classes"])
+        function_id_count = len(inputs["functions"])
 
-        all_actual = []
-        all_real = []
+        while True:
+            question_id = random.choices([i for i in range(self.question_count)], weights=[4, 1, 1, 1, 1])[0]
 
-        question_id = random.randrange(self.question_count)
+            # A is subtype B
+            if question_id == 0:
+                derived_id = random.randrange(class_id_count)
 
-        # A is subtype B
-        while question_id == 0:
-            derived_id = random.randrange(class_id_count)
+                possible_super_types = inputs["classes"][derived_id]["superTypes"]
+                if len(possible_super_types) == 0:
+                    continue
 
-            possible_super_types = inputs["classes"][derived_id]["superTypes"]
-            if len(possible_super_types) == 0:
-                continue
+                true_base_id = random.choice(possible_super_types)
 
-            true_base_id = random.choice(possible_super_types)
+                actual = self.subtype([class_embeddings[derived_id], class_embeddings[true_base_id]])
+                real = 1.0
 
-            actual = self.subtype([class_embeddings[derived_id], class_embeddings[true_base_id]])
-            real = 1.0
+                self.cnt[question_id] += 1
+                return [actual], [real]
 
-            all_actual.append(actual)
-            all_real.append(real)
-            break
+            # A is NOT subtype B
+            if question_id == 1:
+                derived_id = random.randrange(class_id_count)
 
-        # A is NOT subtype B
-        while question_id == 1:
-            derived_id = random.randrange(class_id_count)
+                possible_super_types = inputs["classes"][derived_id]["superTypes"]
+                impossible_super_types = list(filter(lambda t: t not in possible_super_types, [i for i in range(class_id_count)]))
+                if len(impossible_super_types) == 0:
+                    continue
 
-            possible_super_types = inputs["classes"][derived_id]["superTypes"]
-            impossible_super_types = list(filter(lambda t: t not in possible_super_types, [i for i in range(class_id_count)]))
-            if len(impossible_super_types) == 0:
-                continue
+                wrong_base_id = random.choice(impossible_super_types)
 
-            wrong_base_id = random.choice(impossible_super_types)
+                actual = self.subtype([class_embeddings[derived_id], class_embeddings[wrong_base_id]])
+                real = 0.0
 
-            actual = self.subtype([class_embeddings[derived_id], class_embeddings[wrong_base_id]])
-            real = 0.0
+                self.cnt[question_id] += 1
+                return [actual], [real]
 
-            all_actual.append(actual)
-            all_real.append(real)
-            break
+            # A is NOT subtype B, because A is function
+            if question_id == 2:
+                if class_id_count == 0 or function_id_count == 0:
+                    continue
 
-        # todo: A contains as members set X
-        # todo: A NOT contains as members set X
-        # todo: A is callable with set X
-        # todo: A is NOT callable with set X
-        # todo: A(X) is B
-        # todo: A(X) is NOT B
+                derived_id = random.randrange(function_id_count)
+                base_id = random.randrange(class_id_count)
 
-        return all_actual, all_real
+                actual = self.subtype([function_embeddings[derived_id], class_embeddings[base_id]])
+                real = 0.0
+
+                self.cnt[question_id] += 1
+                return [actual], [real]
+
+            # A is NOT subtype B, because B is function
+            if question_id == 3:
+                if class_id_count == 0 or function_id_count == 0:
+                    continue
+
+                derived_id = random.randrange(class_id_count)
+                base_id = random.randrange(function_id_count)
+
+                actual = self.subtype([class_embeddings[derived_id], function_embeddings[base_id]])
+                real = 0.0
+
+                self.cnt[question_id] += 1
+                return [actual], [real]
+
+            # A is NOT subtype B, because A and B are functions
+            if question_id == 4:
+                if function_id_count <= 1:
+                    continue
+
+                derived_id = random.randrange(function_id_count)
+                base_id = random.randrange(function_id_count)
+
+                if base_id == derived_id:
+                    base_id = (base_id + 1) % function_id_count
+
+                actual = self.subtype([function_embeddings[derived_id], function_embeddings[base_id]])
+                real = 0.0
+
+                self.cnt[question_id] += 1
+                return [actual], [real]
+
+    def subtypes_of(self, x, inputs):
+        pass
+
+    def super_properties(self, x, inputs):
+        pass
 
 
 class KInputsNN(keras.Model):
